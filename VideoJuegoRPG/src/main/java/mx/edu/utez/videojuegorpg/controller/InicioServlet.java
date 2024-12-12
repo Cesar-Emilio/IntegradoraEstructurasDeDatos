159d895fdae066331a2191c384f4b1f69dcaaebc
Sebastian Jimenez <20233tn097@utez.edu.mx>
2024-12-11 20:14:28 -0600

    la de equipos ya tiene el generico de personaje, yupi

--- VideoJuegoRPG/src/main/java/mx/edu/utez/videojuegorpg/controller/InicioServlet.java
+++ VideoJuegoRPG/src/main/java/mx/edu/utez/videojuegorpg/controller/InicioServlet.java
@@ -1,4 +1,40 @@
 package mx.edu.utez.videojuegorpg.controller;
 
-public class InicioServlet {
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import mx.edu.utez.videojuegorpg.model.*;
+
+import java.io.IOException;
+
+public class InicioServlet extends HttpServlet {
+
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        Equipo<Personaje> miEquipo = new Equipo<>();
+
+        Rol guerreroRol = new Rol("Guerrero", new ArrayList<Habilidad>() {{
+            add(new Habilidad("Ataque Básico", 10));
+            add(new Habilidad("Habilidad Especial", 20));
+        }});
+
+        Rol magoRol = new Rol("Mago", new ArrayList<Habilidad>() {{
+            add(new Habilidad("Bola de Fuego", 15));
+            add(new Habilidad("Escudo Mágico", 10));
+        }});
+
+        Personaje personaje1 = new Personaje("Aragorn", guerreroRol, 80, 10, 100);
+        Personaje personaje2 = new Personaje("Gandalf", magoRol, 60, 5, 80);
+
+        miEquipo.agregar(personaje1);
+        miEquipo.agregar(personaje2);
+
+        request.setAttribute("miEquipo", miEquipo);
+        request.getRequestDispatcher("equipo.jsp").forward(request, response);
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+    }
 }
--- VideoJuegoRPG/src/main/java/mx/edu/utez/videojuegorpg/model/Equipo.java
+++ VideoJuegoRPG/src/main/java/mx/edu/utez/videojuegorpg/model/Equipo.java
@@ -1,4 +1,116 @@
 package mx.edu.utez.videojuegorpg.model;
 
-public class Equipo {
+public class Equipo<T extends Personaje> {
+    private Nodo<T> cabeza;
+    private int tamanio;
+
+    private static class Nodo<T> {
+        T dato;
+        Nodo<T> siguiente;
+
+        public Nodo(T dato) {
+            this.dato = dato;
+            this.siguiente = null;
+        }
+    }
+
+    public Equipo() {
+        this.cabeza = null;
+        this.tamanio = 0;
+    }
+
+    public void agregar(T personaje) {
+        Nodo<T> nuevo = new Nodo<>(personaje);
+        if (cabeza == null) {
+            cabeza = nuevo;
+        } else {
+            Nodo<T> actual = cabeza;
+            while (actual.siguiente != null) {
+                actual = actual.siguiente;
+            }
+            actual.siguiente = nuevo;
+        }
+        tamanio++;
+    }
+
+    public boolean quitar(T personaje) {
+        if (cabeza == null) return false;
+
+        if (cabeza.dato.equals(personaje)) {
+            cabeza = cabeza.siguiente;
+            tamanio--;
+            return true;
+        }
+
+        Nodo<T> actual = cabeza;
+        while (actual.siguiente != null && !actual.siguiente.dato.equals(personaje)) {
+            actual = actual.siguiente;
+        }
+
+        if (actual.siguiente != null) {
+            actual.siguiente = actual.siguiente.siguiente;
+            tamanio--;
+            return true;
+        }
+        return false;
+    }
+
+    public boolean cambiarPosicion(int indice1, int indice2) {
+        if (indice1 < 0 || indice2 < 0 || indice1 >= tamanio || indice2 >= tamanio) return false;
+
+        if (indice1 == indice2) return true;
+
+        Nodo<T> nodo1 = obtenerNodoPorIndice(indice1);
+        Nodo<T> nodo2 = obtenerNodoPorIndice(indice2);
+
+        if (nodo1 == null || nodo2 == null) return false;
+
+        T temp = nodo1.dato;
+        nodo1.dato = nodo2.dato;
+        nodo2.dato = temp;
+        return true;
+    }
+
+    public void reducirTamanio(int nuevoTamanio) {
+        if (nuevoTamanio < 0 || nuevoTamanio >= tamanio) return;
+
+        Nodo<T> actual = cabeza;
+        for (int i = 1; i < nuevoTamanio; i++) {
+            actual = actual.siguiente;
+        }
+
+        actual.siguiente = null;
+        tamanio = nuevoTamanio;
+    }
+
+    public T obtenerPorIndice(int indice) {
+        Nodo<T> nodo = obtenerNodoPorIndice(indice);
+        return nodo == null ? null : nodo.dato;
+    }
+
+    private Nodo<T> obtenerNodoPorIndice(int indice) {
+        if (indice < 0 || indice >= tamanio) return null;
+
+        Nodo<T> actual = cabeza;
+        for (int i = 0; i < indice; i++) {
+            actual = actual.siguiente;
+        }
+        return actual;
+    }
+
+    public boolean estaVacio() {
+        return tamanio == 0;
+    }
+
+    public int obtenerTamanio() {
+        return tamanio;
+    }
+
+    public void imprimirEquipo() {
+        Nodo<T> actual = cabeza;
+        while (actual != null) {
+            System.out.println(actual.dato);
+            actual = actual.siguiente;
+        }
+    }
 }
