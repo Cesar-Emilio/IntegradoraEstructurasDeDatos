1be47f559ce781d0776eb680e6f00f2103e4a108
Erick <20233tn060@utez.edu.mx>
2024-12-12 05:30:53 -0600

    Erick - Nivel y batalla

--- VideoJuegoRPG/src/main/java/mx/edu/utez/videojuegorpg/model/Nivel.java
+++ VideoJuegoRPG/src/main/java/mx/edu/utez/videojuegorpg/model/Nivel.java
@@ -1,4 +1,10 @@
 package mx.edu.utez.videojuegorpg.model;
 
+import mx.edu.utez.videojuegorpg.dataStructures.ArrayList;
+
 public class Nivel {
+    public ArrayList<Enemigo> obtenerEnemigosNivel(int nivel) {
+        return EnemigosConfig.obtenerEnemigos(nivel);
+
+    }
 }
--- VideoJuegoRPG/src/main/java/mx/edu/utez/videojuegorpg/util/Batalla.java
+++ VideoJuegoRPG/src/main/java/mx/edu/utez/videojuegorpg/util/Batalla.java
@@ -1,6 +1,17 @@
 package mx.edu.utez.videojuegorpg.util;
 
-public class Batalla {
+import mx.edu.utez.videojuegorpg.model.Enemigo;
+import mx.edu.utez.videojuegorpg.model.Habilidad;
+import mx.edu.utez.videojuegorpg.model.Personaje;
 
+public class Batalla{
+    public void calcularDañoEnemigo(Habilidad habilidad, Enemigo enemigo) {
+        enemigo.setSalud((habilidad.getDaño() * 10)/(5 + enemigo.getDefensa()));
 
+    }
+
+    public void calcularDañoAliado(Habilidad habilidad, Personaje aliado) {
+        aliado.setSalud((habilidad.getDaño() * 10)/(5 + aliado.getDefensa()));
+
+    }
 }
